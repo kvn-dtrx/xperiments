f2287e15e42abe0fc6ad3235c132e51409678f92
Kevin Dietrich <kvndtr@icloud.com>
2025-04-24 22:17:58 +0200

    Temporary commit at 2025-04-24T22:17:58, please squash

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 failed=0
-find . -name "*.ipynb" -print0 | while IFS='' read -r -d '' notebook; do
+while IFS='' read -r -d '' notebook; do
   if [ -n "$(jq '.cells[] | select(.outputs | length > 0)' "$notebook")" ]; then
     echo "The notebook ${notebook} contains uncleared output cells and should not be committed!"
     failed=1
   fi
-done
+done < <(find . -name "*.ipynb" -print0)
 
 if [ "${failed}" -gt 0 ]; then
   exit 1
