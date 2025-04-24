529fb34eb31c0250e7b0f2333504474b3fd98030
Kevin Dietrich <kvndtr@icloud.com>
2025-04-24 22:12:02 +0200

    fo ba ba

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+failed=0
+find . -name "*.ipynb" -print0 | while IFS='' read -r -d '' notebook; do
+  if [ -n "$(jq '.cells[] | select(.outputs | length > 0)' "$notebook")" ]; then
+    echo "The notebook ${notebook} contains uncleared output cells and should not be committed!"
+    failed=1
+  fi
+done
+
+if [ "${failed}" -gt 0 ]; then
+  exit 1
+else
+  echo "No uncleared output cells found."
+  exit 0
+fi
