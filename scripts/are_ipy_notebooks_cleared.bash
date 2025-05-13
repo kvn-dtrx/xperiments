735c70ec3802de5f2a4d2e7fa186b7f1faa987e3
Kevin Dietrich <kvndtr@icloud.com>
2025-05-13 23:19:18 +0200

    wip: Intermediate Commit @ 2025-05-13T23:19:18+02:00

--- scripts/are_ipy_notebooks_cleared.bash
+++ scripts/are_ipy_notebooks_cleared.bash
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
 # ---
-# description: Check if Jupyter notebooks have been cleared of output cells.
+# description: |
+#   Check if Jupyter notebooks have cleared output cells.
+#   This script is intended to be run in a GitHub Actions workflow.
 # ---
 
 failed=0
+
+notebooks="$(
+    git diff --name-only --diff-filter=AM -z "${BASE_SHA}" "${HEAD_SHA}" |
+        grep -z '\.ipynb$' || true
+)"
 
 while IFS='' read -r -d '' notebook; do
     if [ -n "$(jq '.cells[] | select(.outputs | length > 0)' "${notebook}")" ]; then
@@ -14,12 +21,7 @@ while IFS='' read -r -d '' notebook; do
     fi
     # In order to alter the state variable, the loop must be run in the main shell
     # why process substitution is used here.
-done < <(
-    git diff --name-only --diff-filter=AM -z origin/"${BASE_REF}"...HEAD |
-        grep -z '\.ipynb$' ||
-        true
-    # find . -name "*.ipynb" -print0
-)
+done <<<"${notebooks}"
 
 if [ "${failed}" -gt 0 ]; then
     exit 1
